7bd6b9a0ae113869e27eb0d2c2b88939732607d1
Matt Fields <mafields@ncsu.edu>
2019-03-19 13:53:11 -0400

    Add default command for unsupported OS. Fixes #1

--- lib/facter/fastx_version.rb
+++ lib/facter/fastx_version.rb
@@ -1,5 +1,7 @@
 Facter.add('fastx_version') do
   setcode do
+    cmd = "/bin/false" # Default to nothing
+
     case Facter.value(:operatingsystem)
     when 'Ubuntu', 'Debian'
       cmd = "/usr/bin/dpkg-query --showformat='${Version}' --show starnetfastx2"
