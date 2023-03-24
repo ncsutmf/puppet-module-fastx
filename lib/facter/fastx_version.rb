492364ff0f4c02154f3513c3c4e01d9344f3938b
Tom Farwig <tmfarwig@ncsu.edu>
2023-03-24 10:38:46 -0400

    update fact for debian w/ new package name

--- lib/facter/fastx_version.rb
+++ lib/facter/fastx_version.rb
@@ -4,7 +4,7 @@ Facter.add('fastx_version') do
 
     case Facter.value(:operatingsystem)
     when 'Ubuntu', 'Debian'
-      cmd = "/usr/bin/dpkg-query --showformat='${Version}' --show starnetfastx2"
+      cmd = "/usr/bin/dpkg-query --showformat='${Version}' --show starnetfastx2 || /usr/bin/dpkg-query --showformat='%{Version}' --show fastx-server"
     when 'CentOS', 'RedHat', 'Fedora'
       cmd = '/bin/rpm -q StarNetFastX2 > /dev/null && /bin/rpm -q --qf "%{VERSION}" StarNetFastX2 || /bin/rpm -q --qf "%{VERSION}" fastx-server'
     end
